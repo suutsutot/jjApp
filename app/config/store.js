45a4a03e34b01c291fc56795c5654722450dbaad
Mark Vnukov <mark.vnukov@nrg-soft.com>
2018-06-01 16:12:42 +0300

    work with logger

--- app/config/store.js
+++ app/config/store.js
@@ -1,16 +1,32 @@
-import * as redux from 'redux'
-import thunk from 'redux-thunk'
-import {createLogger} from 'redux-logger'
-import reducers from 'app/data'
-
+import * as redux from 'redux';
+import thunk from 'redux-thunk';
+import {createLogger} from 'redux-logger';
+import reducers from 'app/data';
 import {middleware} from 'app/config/redux';
 
-const logger = createLogger();
+// const logger = createLogger({});
+
+const logger = store => next => action => {
+    console.log('dispatching', action);
+    let result = next(action);
+    console.log('next state', store.getState());
+
+    // start
+
+    // let data = [{
+    //     'dispatching': action,
+    //     'next state': store.getState()
+    // }];
+
+    // end
+
+    return result
+};
 
 let initialState = {};
 
 let store = redux.createStore(reducers, initialState, redux.compose(
-    redux.applyMiddleware(logger, thunk, middleware)
+    redux.applyMiddleware(thunk, middleware, logger)
 ));
 
 export default store
