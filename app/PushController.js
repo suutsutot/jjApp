867ab49f5c764d13ea9871470f3331f640f6fe33
dsvgit <dsv.mail@yandex.ru>
2018-08-25 16:51:46 +0300

    jj-579. push notifications - added remote notifications from firebase, updated gradle version

--- app/PushController.js
+++ app/PushController.js
@@ -4,9 +4,15 @@ import PushNotification from 'react-native-push-notification';
 export default class PushController extends Component {
     componentDidMount() {
         PushNotification.configure({
-            onNotification: function (notification) {
-                console.log('NOTIFICATION:', notification);
+            onRegister: function(token) {
+                console.log( 'TOKEN:', token );
             },
+            onNotification: function(notification) {
+                console.log( 'NOTIFICATION:', notification );
+            },
+            senderID: '490595239693',
+            popInitialNotification: true,
+            requestPermissions: true,
         });
     }
 
