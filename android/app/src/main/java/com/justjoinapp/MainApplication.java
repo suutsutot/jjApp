034e465b7ef7e10a09f097b3fb7788525009e713
dsvgit <dsv.mail@yandex.ru>
2018-11-09 17:22:23 +0300

    added crashlytics fabric integration

--- android/app/src/main/java/com/justjoinapp/MainApplication.java
+++ android/app/src/main/java/com/justjoinapp/MainApplication.java
@@ -2,6 +2,7 @@ package com.justjoinapp;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.facebook.react.ReactApplication;
 import com.azendoo.reactnativesnackbar.SnackbarPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
@@ -13,6 +14,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import io.fabric.sdk.android.Fabric;
 import java.util.Arrays;
 import java.util.List;
 
@@ -50,6 +52,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    Fabric.with(this, new Crashlytics());
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
