0aeff46f01995b4e0999ac78fd745f86fa2b4f86
dsvgit <dsv.mail@yandex.ru>
2018-11-10 20:57:05 +0300

    reset whole state on logout

--- app/data/reducers.js
+++ app/data/reducers.js
@@ -2,6 +2,7 @@ import {combineReducers} from 'redux';
 import { persistReducer } from 'redux-persist';
 import { AsyncStorage } from 'react-native';
 
+import { LOGOUT } from 'app/constants/actionTypes';
 import {activityReducer} from './activity/activityReducer';
 import {authorize} from './authorization/reducer';
 import {globalReducer} from './global/globalReducer';
@@ -10,7 +11,7 @@ import {navReducer} from './nav/navReducer';
 import {events} from './event/reducer';
 import {notifications} from './notifications/reducers';
 
-export default combineReducers({
+const appReducer = combineReducers({
   nav: navReducer,
   activity: activityReducer,
   authorize: persistReducer({
@@ -27,3 +28,11 @@ export default combineReducers({
     whitelist: ['list', 'data']
   }, notifications),
 });
+
+export default (state, action) => {
+  if (action.type === LOGOUT) {
+    appReducer(undefined, action);
+  }
+
+  return appReducer(state, action);
+}
